241b695fed62a4cef198afd2f419fda562a4ab0e
Tapok322 <146731387+Tapok322@users.noreply.github.com>
2024-10-23 13:47:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите исходное число для конвертирования:");
+string x = Console.ReadLine();
+
+Console.WriteLine("Введите основание исходной системы счисления:");
+int q1;
+if (!int.TryParse(Console.ReadLine(), out q1))
+{
+    Console.WriteLine("Ошибка: указана некорректная исходная система счисления.");
+    return;
+}
+Console.WriteLine("Введите основание целевой системы счисления:");
+int q2; 
+if (!int.TryParse(Console.ReadLine(), out q2) || q2 < 2)
+{
+    Console.WriteLine("Ошибка: указана некорректная целевая система счисления");
+    return;
+}
+
+try
+{
+    int decimalNumber = Convert.ToInt32(x, q1);
+    string result = Convert.ToString(decimalNumber, q2);
+    Console.WriteLine($"Результат: {result}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Ошибка: Введено некорректное число для указанной системы счисления.");
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("Ошибка: Указана некорректная система счисления.");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("Ошибка: Число слишком велико для конвертации.");
+}
